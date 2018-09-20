e9a2d22fa231c82c914be2dffa943100b15bd52a
Mihkel Tiganik <mtiganik@gmail.com>
2018-09-20 13:03:05 +0300

    Initial commit

---
+++ src/com/company/IndexController.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Model m){
+        m.addAttribute("someAttribute", "Hello World");
+        return "index12";
+    }
+}
---
+++ web/WEB-INF/views/index12.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Mihkel Tiganik
+  Date: 20-Sep-18
+  Time: 12:57 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<p>from index12 ${someAttribute}</p>
+</body>
+</html>
